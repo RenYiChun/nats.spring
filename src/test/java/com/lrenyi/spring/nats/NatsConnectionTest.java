55410031efded1eda73fed6afd8dc13ce3846634
RenYiChun <lrenyi.com@gmail.com>
2026-04-14 18:16:00 +0800

    更新版本号

--- src/test/java/com/lrenyi/spring/nats/NatsConnectionTest.java
+++ src/test/java/com/lrenyi/spring/nats/NatsConnectionTest.java
@@ -6,6 +6,7 @@ import io.nats.client.Message;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class NatsConnectionTest {
